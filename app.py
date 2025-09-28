5bb57e48fbd1892170608bb2d70dba0387655474
30lima <pedrolimaa354@gmail.com>
2025-09-28 18:41:08 -0300

    refactor: clean up app.py structure

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask
-from routes import bp
-from mqtt_client import start_mqtt
+from app import create_app
+from app.mqtt_client import start_mqtt
 
-app = Flask(__name__)
-app.register_blueprint(bp)
-
-# Inicia o MQTT antes de rodar o Flask
+app = create_app()
 start_mqtt()
-    
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
